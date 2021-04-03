ca80f6cb6290ab21877e49469c11d6116f4b8340
AnotherFilthyCasual <50429373+AnotherFilthyCasual@users.noreply.github.com>
2021-04-03 15:49:37 -0500

    Added Reducer
    Added the reducer function of the sequential code that reduces the individual maps. Also added in a loop over all of the text files

--- main.cpp
+++ main.cpp
@@ -8,7 +8,10 @@
 #include <sstream>
 
 using namespace std;
+
+const int numFiles = 16;
 queue<string> words;
+unordered_map<string, int> masterMap;
 
 void Reader(ifstream *file) {
 	string text;
@@ -36,17 +39,53 @@ unordered_map<string, int> Mapper() {
 	return map;
 }
 
+void ReduceMapper(unordered_map<string,int> *map) {
+	for (auto i : *map)
+	{
+		masterMap[i.first] += i.second;
+	}
+}
+
 int main(int argc, char **argv) {
     ifstream file;
-    file.open("files/1.txt");
- 
+    
+	for (int i = 1; i < numFiles+1; i++)
+	{
+		if (i != 9 && i != 10)
+		{
+			cout << i << ".txt is running." << endl;
+			ostringstream stream;
+			stream << "files/" << i << ".txt";
+			string fileName = stream.str();
+			file.open(fileName);
+			Reader(&file);
+			cout << "Lines in Queue: " << words.size() << endl;
+			unordered_map<string, int> map = Mapper();
+			cout << "Map length: " << map.size() << endl;
+			ReduceMapper(&map);
+			cout << "Master Map length: " << masterMap.size() << endl;
+		}
+	}
+
+
+	/*file.open("files/1.txt");
 	Reader(&file);
-	cout << words.size();
+	cout << words.size() << endl;
 	unordered_map<string, int> map = Mapper();
+	
+	file.open("files/2.txt");
+	Reader(&file);
+	cout << words.size() << endl;
+	unordered_map<string, int> map2 = Mapper();
     for (auto x : map) {
         cout << x.first << ": " << x.second << endl;
     }
     cout << "Map length: " << map.size() << endl;
+	cout << "Map2 length: " << map2.size() << endl;
+
+	ReduceMapper(&map);
+	ReduceMapper(&map2);
+
+	cout << "Master Map length: " << masterMap.size() << endl;*/
 
 }
-
