0047858d211588049e5208de721cb3b694f64402
Prateek Gupta <gupta198@ecegrid-thin1.ecn.purdue.edu>
2021-04-03 17:53:53 -0400

    Finished Sequential Imlementation

--- main.cpp
+++ main.cpp
@@ -9,83 +9,66 @@
 
 using namespace std;
 
-const int numFiles = 16;
-queue<string> words;
-unordered_map<string, int> masterMap;
 
-void Reader(string fileName) {
+void Reader(string fileName, queue<string> *words) {
     ifstream file;
     file.open(fileName);
     string text;
 
     while (getline(file, text)) {
-        words.push(text);
+        words->push(text);
     }
     file.close();
 }
 
-unordered_map<string, int> Mapper() {
+unordered_map<string, int> Mapper(queue<string> *words) {
     unordered_map<string, int> map;
-    while (!words.empty())
-    {
+    while (!words->empty()) {
         string wrd;
-        stringstream iss(words.front());
-        while (iss >> wrd)
-        {
+        stringstream iss(words->front());
+        while (iss >> wrd) {
             map[wrd]++;
         }
-        words.pop();
-
+        words->pop();
     }
 
     return map;
 }
 
-void ReduceMapper(unordered_map<string,int> *map) {
-    for (auto i : *map)
-    {
-        masterMap[i.first] += i.second;
+void ReduceMapper(unordered_map<string,int> *map, unordered_map<string, int> *masterMap) {
+    for (auto i : *map) {
+        (*masterMap)[i.first] += i.second;
     }
 }
 
 int main(int argc, char **argv) {
+    const int NUM_FILES = 16;
+    const int NUM_MAPS = 3;
+    queue<string> words;
+    unordered_map<string, int> masterMap;
+    unordered_map<string, int> map[NUM_MAPS];
 
-    for (int i = 1; i < numFiles+1; i++)
-    {
-        if (i != 9 && i != 10)
-        {
-            cout << i << ".txt is running." << endl;
-            ostringstream stream;
-            stream << "files/" << i << ".txt";
-            string fileName = stream.str();
-            Reader(fileName);
-            cout << "Lines in Queue: " << words.size() << endl;
-            unordered_map<string, int> map = Mapper();
-            cout << "Map length: " << map.size() << endl;
-            ReduceMapper(&map);
-            cout << "Master Map length: " << masterMap.size() << endl;
+    //1 reader per file
+    for (int i = 1; i <= NUM_FILES; i++) {
+        if (i == 9 && i == 10) {
+            continue;
         }
+        cout << i << ".txt is running." << endl;
+        ostringstream stream;
+        stream << "files/" << i << ".txt";
+        string fileName = stream.str();
+        Reader(fileName, &words);
+        cout << "Lines in Queue: " << words.size() << endl;
     }
 
-
-    /*file.open("files/1.txt");
-      Reader(&file);
-      cout << words.size() << endl;
-      unordered_map<string, int> map = Mapper();
-
-      file.open("files/2.txt");
-      Reader(&file);
-      cout << words.size() << endl;
-      unordered_map<string, int> map2 = Mapper();
-      for (auto x : map) {
-      cout << x.first << ": " << x.second << endl;
-      }
-      cout << "Map length: " << map.size() << endl;
-      cout << "Map2 length: " << map2.size() << endl;
-
-      ReduceMapper(&map);
-      ReduceMapper(&map2);
-
-      cout << "Master Map length: " << masterMap.size() << endl;*/
-
+    for (int i = 0; i < NUM_MAPS; i++) {
+        map[i] = Mapper(&words);
+        cout << "Map length: " << map[i].size() << endl;
+    }
+    //barrier
+    for (int i = 0; i < NUM_MAPS; i++) {
+        ReduceMapper(&map[i], &masterMap);
+    }
+    cout << "Master Map length: " << masterMap.size() << endl;
+    return(EXIT_SUCCESS);
 }
