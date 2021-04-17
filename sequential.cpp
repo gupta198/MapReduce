51b540d77dc0f7eec93f570fad29d91612bc8c5b
Prateek Gupta <gupta198@scholar-fe06.rcac.purdue.edu>
2021-04-17 16:44:35 -0400

    added sequential implementation

---
+++ sequential.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unordered_map>
+#include <string.h>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <omp.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+   const int NUM_FILES = 14;
+   unordered_map<string, int> map;
+   double time = -omp_get_wtime();
+
+   for (int i = 1; i <= NUM_FILES; i++) {
+      ostringstream stream;
+      stream << "files/" << i << ".txt";
+      string fileName = stream.str();
+      ifstream file;
+      file.open(fileName);
+      string word;
+      while (file >> word) {
+         map[word]++;
+      }
+   }
+
+   time += omp_get_wtime();
+   cout << "Map length is: " << map.size() << endl;
+   cout << "Time taken: " << time << endl;
+   return(EXIT_SUCCESS);
+}
