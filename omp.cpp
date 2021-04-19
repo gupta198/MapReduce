23a32309c1f883c16c25b76ff4e36fb4e3110f8e
AnotherFilthyCasual <50429373+AnotherFilthyCasual@users.noreply.github.com>
2021-04-19 12:53:12 -0500

    Updated omp.cpp
    fixed a couple of issues with omp_get_max_thread also made a way to have reduce threads be somewhat parallel. Added some cout statements to give some timing information.

--- omp.cpp
+++ omp.cpp
@@ -14,11 +14,15 @@ void Reader(string fileName, queue<string> *words) {
    ifstream file;
    file.open(fileName);
    string text;
-
-   while (getline(file, text)) {
+   if (file) {
+     ostringstream ss;
+     ss << file.rdbuf();
+     text = ss.str();
+   }
+  // while (getline(file, text)) {
       #pragma omp critical
       words->push(text);
-   }
+  // }
    file.close();
 }
 
@@ -43,15 +47,22 @@ unordered_map<string, int> Mapper(queue<string> *words) {
 }
 
 void ReduceMapper(unordered_map<string,int> *map, unordered_map<string, int> *masterMap) {
+   unordered_map<string,int>::iterator it;
    for (auto i : *map) {
-      #pragma omp atomic
-      (*masterMap)[i.first] += i.second;
+      it = (*masterMap).find(i.first);
+      if (it != (*masterMap).end()){
+        #pragma omp atomic
+        it->second += i.second;
+      } else {
+        #pragma omp critical
+        (*masterMap)[i.first] += i.second;
+      }
    }
 }
 
 int main(int argc, char **argv) {
-   const int NUM_FILES = 250;
-   const int NUM_MAPS = omp_get_num_threads();
+   const int NUM_FILES = 1000;
+   const int NUM_MAPS = omp_get_max_threads();
    queue<string> words;
    unordered_map<string, int> masterMap;
    unordered_map<string, int> map[NUM_MAPS];
@@ -69,24 +80,25 @@ int main(int argc, char **argv) {
       Reader(fileName, &words);
       //cout << "Lines in Queue: " << words.size() << endl;
    }
-   //remove this barrier 
-   #pragma omp parallel
-   {
-      #pragma omp single
+   //remove this barrier
+   double timeRead = time + omp_get_wtime();
+   cout << "Queue size: " << words.size() << endl;
+   cout << "All files Read.... Time taken to Read Files: " << timeRead << endl; 
+   #pragma omp parallel for
       for (int i = 0; i < NUM_MAPS; i++) {
-         #pragma omp task
-         {
             map[i] = Mapper(&words);
             //cout << "Map length: " << map[i].size() << endl;
-         }
       }
-   }
    //barrier
-   cout << "Execution time: " << time+omp_get_wtime() << endl;
+   cout << "Finished Mapping Queue... Time taken to Map Queue: " << time + omp_get_wtime()-timeRead << endl;
+   //cout << "Execution time: " << time+omp_get_wtime() << endl;
+   masterMap = map[0];
    #pragma omp parallel for
-   for (int i = 0; i < NUM_MAPS; i++) {
+   for (int i = 1; i < NUM_MAPS; i++) {
       ReduceMapper(&map[i], &masterMap);
    }
+   cout << "Finished Reducing Maps..." << endl << endl << endl;
+   cout << "**OpenMP Implementation**" << endl << "Number of threads: " << omp_get_max_threads() << endl;
    cout << "Master Map length: " << masterMap.size() << endl;
    cout << "Execution time: " << time+omp_get_wtime() << endl;
    return(EXIT_SUCCESS);
